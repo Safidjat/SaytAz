4d6c0d8c0524e28f487b728702ef132b493542ce
Safidjat <safidjat.isayeva.04@gmail.com>
2025-03-28 21:12:28 +0400

    el

--- js/fixed.js
+++ js/fixed.js
@@ -1,6 +1,7 @@
 const umFixed=document.getElementById('umFixed');
 const xMark=document.getElementById('xMark');
-
+const ell=document.getElementById('ell');
 function yigisdir(div){
     div.style.display='none';
+    ell.style.display='none';
 }
